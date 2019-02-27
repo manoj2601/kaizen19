a5b35f16480b7ce3abbc67300e03771b32bbea47
manoj2601 <45541010+manoj2601@users.noreply.github.com>
2019-02-27 14:15:39 +0530

    Information of hospitals added.

--- program.cpp
+++ program.cpp
@@ -25,3 +25,23 @@ int main()
      cout << "please mention the problem you are facing? please here refer general issue like fever and body pain as opd and other specific issue by simply naming the body part"<<endl;
      string problem ;
      cin >>problem;
+
+       map < string , string > hos_op;
+
+    hos_op["hospital_1"]= "eye opd ear stomach bone skin";
+    hos_op["hospital_2"]= "eye opd ear stomach bone dental";
+    hos_op["hospital_3"]= "eye opd ear stomach skin nursing";
+    hos_op["hospital_4"]= "eye opd ear stomach dental blood";
+    hos_op["hospital_5"]= "eye opd ear stomach bone skin";
+
+    vector <int> v1t; v1t.push_back(700); v1t.push_back(2200);
+   vector <int>v2t; v2t.push_back(0); v2t.push_back(0);
+   vector <int>v3t; v3t.push_back(430); v3t.push_back(2330);
+   vector <int>v4t; v4t.push_back(600); v4t.push_back(450);
+   vector <int>v5t; v5t.push_back(0); v5t.push_back(0);
+
+   vector <int>v1; v1.push_back(5); v1.push_back(6);
+   vector <int>v2;v2.push_back(2); v2.push_back(0); v2.push_back(0);
+   vector <int>v3; v3.push_back(1); v3.push_back(8);
+   vector <int>v4; v4.push_back(7); v4.push_back(3);
+   vector <int>v5; v5.push_back(0); v5.push_back(11);
